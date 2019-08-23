68c1fc0d24978269f87ee624e9d689f158ac31f3
david9705 <abc88662a@gmail.com>
2019-08-24 02:24:53 +0800

    Add files via upload

---
+++ speed_crawler_5N/DAI.py
@@ -0,0 +1,156 @@
+import time, DAN, requests, random , json , math
+import socket, re
+from datetime import datetime
+
+ServerURL = 'http://140.113.199.188' #with no secure connection
+#ServerURL = 'https://DomainName' #with SSL connection
+Reg_addr = 'team3final' #if None, Reg_addr = MAC address
+
+DAN.profile['dm_name']='highwayN'
+DAN.profile['df_list']=['highwayN']
+DAN.profile['d_name']= None # None for autoNaming
+DAN.device_registration_with_retry(ServerURL, Reg_addr)
+time.sleep(10)
+class my_dictionary(dict):
+  
+    # __init__ function  
+    def __init__(self):  
+        self = dict()  
+          
+    # Function to add key:value  
+    def add(self, key, value):  
+        self[key] = value
+
+with open('fixN.json','r') as reader:
+    jf = json.loads(reader.read())
+
+def get_first_speedmsg():
+    #request msg first 40km
+    msgFromClient = "國5 N 15.0"
+    bytesToSend = str.encode(msgFromClient)
+    serverAddressPort = ("140.113.203.216",54710)
+    bufferSize = 4096
+    #create a UDP socket form client side
+    UDPClientSocket = socket.socket(family = socket.AF_INET,type = socket.SOCK_DGRAM)
+    #Send to server using created UDP socket
+    UDPClientSocket.sendto(bytesToSend,serverAddressPort)
+    UDPClientSocket.settimeout(3)
+    try:
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    except socket.timeout as error:
+        UDPClientSocket.close()
+        print("first speed msg udp package lost")
+        get_first_speedmsg()
+    msg = "{} ".format(msgFromServer[0])
+    UDPClientSocket.close()
+    return msg
+
+def get_second_speedmsg():
+    #request msg next 40km
+    msgFromClient = "國5 N 55.0"
+    bytesToSend = str.encode(msgFromClient)
+    serverAddressPort = ("140.113.203.216",54710)
+    bufferSize = 4096
+    #create a UDP socket form client side
+    UDPClientSocket = socket.socket(family = socket.AF_INET,type = socket.SOCK_DGRAM)
+    #Send to server using created UDP socket
+    UDPClientSocket.sendto(bytesToSend,serverAddressPort)
+    UDPClientSocket.settimeout(3)
+    try:
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    except socket.timeout as error:
+        print("second speed msg udp package lost")
+        UDPClientSocket.close()
+        get_second_speedmsg()
+    msg = "{} ".format(msgFromServer[0])
+    UDPClientSocket.close()
+    return msg
+
+while True:
+    try:
+        s_15 = True  #0~15kmp
+        s_55 = False #15~50kmp
+        if(s_15):
+            s_55 = True
+           
+            newmsg = get_first_speedmsg()
+
+            newmsg = newmsg.replace("n","i")
+            newmsg = newmsg.replace("t","j")
+            newmsg = newmsg.replace("\i",",")
+            newmsg = newmsg.replace("\j",":")
+            first_fifteen = newmsg.split(',')   #first_forty store前40km data
+            first_fifteen.pop()
+            first_fifteen.pop(0)
+            #print(first_fifteen)
+
+            first_fifteen_d = my_dictionary();  #use dictionary to store kmp/speed
+            for i in range(len(first_fifteen)):
+                kmp1 = first_fifteen[i].split(':')
+                first_fifteen_d.add(math.floor(float(kmp1[0])),kmp1[1])
+            #print(first_fifteen_d)
+        if(s_55):
+            
+            newmsg1 = get_second_speedmsg()
+
+            newmsg1 = newmsg1.replace("n","i")
+            newmsg1 = newmsg1.replace("t","j")
+            newmsg1 = newmsg1.replace("\i",",")
+            newmsg1 = newmsg1.replace("\j",":")
+            second_55 = newmsg1.split(',')   #first_forty store前40km data
+            second_55.pop()
+            second_55.pop(0)
+
+            second_55_d = my_dictionary();  #use dictionary to store kmp/speed
+            for i in range(len(second_55)):
+                kmp1 = second_55[i].split(':')
+                second_55_d.add(math.floor(float(kmp1[0])),kmp1[1])
+            #print(second_55_d)
+        print("-----------json match------------")
+        a = len(jf['segmentList'])
+        for i in range(0,a):
+            b = len(jf['segmentList'][i]['nodeList'])
+            for j in range(0,b):
+                #print((jf['segmentList'][i]['nodeList'][j][3]))
+                ID = jf['segmentList'][i]['nodeList'][j][0]
+                lat = jf['segmentList'][i]['nodeList'][j][1]
+                log = jf['segmentList'][i]['nodeList'][j][2]
+                kmp = math.floor(jf['segmentList'][i]['nodeList'][j][3])
+                #print(type(kmp))
+                if(kmp<=15):
+                    for k,v in first_fifteen_d.items():
+                        if (kmp == k):
+                            realtimespeed = v
+                            break;
+                        else:
+                            if(kmp > k):
+                                realtimespeed = v
+                                break;
+                if(51>=kmp>15):
+                    for k,v in second_55_d.items():
+                        if(kmp == k):
+                            realtimespeed = v
+                            break;
+                        else:
+                            if(kmp>k):  #和國5S不一樣因為里程是由大到小
+                                realtimespeed = v
+                                break;
+                if(kmp>51):
+                    realtimespeed = list(second_55_d.values())[0]
+                print(ID,lat,log,kmp,realtimespeed,datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                DAN.push ('highwayN', lat, log,"國五N_kmp"+str(kmp),realtimespeed, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                time.sleep(0.1)
+        print("-------------------------------------------------")
+        first_fifteen_d.clear()
+        second_55_d.clear()
+        time.sleep(180)
+
+    except Exception as e:
+        print(e)
+        if str(e).find('mac_addr not found:') != -1:
+            print('Reg_addr is not found. Try to re-register...')
+            DAN.device_registration_with_retry(ServerURL, Reg_addr)
+        else:
+            print('Connection failed due to unknow reasons.')
+            time.sleep(1)    
+    time.sleep(0.2)
---
+++ speed_crawler_5S/DAI.py
@@ -0,0 +1,154 @@
+import time, DAN, requests, random , json , math
+import socket, re
+from datetime import datetime
+
+ServerURL = '140.113.199.188' #with no secure connection
+#ServerURL = 'https://DomainName' #with SSL connection
+Reg_addr = 'highwayS' #if None, Reg_addr = MAC address
+
+DAN.profile['dm_name']='highwayS'
+DAN.profile['df_list']=['highwayS']
+DAN.profile['d_name']= None  # None for autoNaming
+DAN.device_registration_with_retry(ServerURL, Reg_addr)
+
+time.sleep(10)
+class my_dictionary(dict):  
+  
+    # __init__ function  
+    def __init__(self):  
+        self = dict()  
+          
+    # Function to add key:value  
+    def add(self, key, value):  
+        self[key] = value
+
+with open('fix1.json','r') as reader:
+    jf = json.loads(reader.read())
+
+def get_first_speedmsg():
+    #request msg first 40km
+    msgFromClient = "國5 S 0.0"
+    bytesToSend = str.encode(msgFromClient)
+    serverAddressPort = ("140.113.203.216",54710)
+    bufferSize = 4096
+    #create a UDP socket form client side
+    UDPClientSocket = socket.socket(family = socket.AF_INET,type = socket.SOCK_DGRAM)
+    #Send to server using created UDP socket
+    UDPClientSocket.sendto(bytesToSend,serverAddressPort)
+    UDPClientSocket.settimeout(3)
+    try:
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    except socket.timeout as error:
+        print("first speed msg udp package lost")
+        UDPClientSocket.close()
+        get_first_speedmsg()
+    msg = "{} ".format(msgFromServer[0])
+    UDPClientSocket.close()
+    return msg
+
+def get_second_speedmsg():
+    #request msg next 40km
+    msgFromClient = "國5 S 40.0"
+    bytesToSend = str.encode(msgFromClient)
+    serverAddressPort = ("140.113.203.216",54710)
+    bufferSize = 4096
+    #create a UDP socket form client side
+    UDPClientSocket = socket.socket(family = socket.AF_INET,type = socket.SOCK_DGRAM)
+    #Send to server using created UDP socket
+    UDPClientSocket.sendto(bytesToSend,serverAddressPort)
+    UDPClientSocket.settimeout(3)
+    try:
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    except socket.timeout as error:
+        print("second speed msg udp package lost")
+        UDPClientSocket.close()
+        get_second_speedmsg()
+    msg = "{} ".format(msgFromServer[0])
+    UDPClientSocket.close()
+    return msg
+    
+while True:
+    try:
+    #Push data to a device feature called "Dummy_Sensor"
+        s_0 = True
+        s_40 = False
+        if(s_0):
+            s_40 = True
+            
+            newmsg = get_first_speedmsg()
+
+            newmsg = newmsg.replace("n","i")
+            newmsg = newmsg.replace("t","j")
+            newmsg = newmsg.replace("\i",",")
+            newmsg = newmsg.replace("\j",":")
+            first_forty = newmsg.split(',')   #first_forty store前40km data
+            first_forty.pop()
+            first_forty.pop(0)
+            #print(first_forty)
+
+            first_forty_d = my_dictionary();  #use dictionary to store kmp/speed
+            for i in range(len(first_forty)):
+                kmp1 = first_forty[i].split(':')
+                first_forty_d.add(math.floor(float(kmp1[0])),kmp1[1])
+            
+        if(s_40):
+            
+            newmsg1 = get_second_speedmsg()
+
+            newmsg1 = newmsg1.replace("n","i")
+            newmsg1 = newmsg1.replace("t","j")
+            newmsg1 = newmsg1.replace("\i",",")
+            newmsg1 = newmsg1.replace("\j",":")
+            second_forty = newmsg1.split(',')   #first_forty store前40km data
+            second_forty.pop()
+            second_forty.pop(0)
+            #print(second_forty)
+
+            second_forty_d = my_dictionary();  #use dictionary to store kmp/speed
+            for i in range(len(second_forty)):
+                kmp1 = second_forty[i].split(':')
+                second_forty_d.add(math.floor(float(kmp1[0])),kmp1[1])
+
+        a = len(jf['segmentList'])
+        for i in range(0,a):
+            b = len(jf['segmentList'][i]['nodeList'])
+            for j in range(0,b):
+                ID = jf['segmentList'][i]['nodeList'][j][0]
+                lat = jf['segmentList'][i]['nodeList'][j][1]
+                log = jf['segmentList'][i]['nodeList'][j][2]
+                kmp = math.floor(jf['segmentList'][i]['nodeList'][j][3])
+                if(kmp<40):
+                    for k,v in first_forty_d.items():
+                        if (kmp == k):
+                            realtimespeed = v
+                            break;
+                        else:
+                            if(kmp < k):
+                                realtimespeed = v;
+                                break;
+                else:
+                    for k,v in second_forty_d.items():
+                        if(kmp == k):
+                            realtimespeed = v
+                            break;
+                        else:
+                            if(kmp<k):
+                                realtimespeed = v
+                                break;
+                print(ID,lat,log,kmp,realtimespeed,datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                DAN.push ('highwayS', lat, log,"國五S_kmp"+str(kmp),realtimespeed, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                time.sleep(0.1)
+        print("------------------------------------------------")
+        first_forty_d.clear()
+        second_forty_d.clear()
+        time.sleep(180)
+
+    except Exception as e:
+        print(e)
+        if str(e).find('mac_addr not found:') != -1:
+            print('Reg_addr is not found. Try to re-register...')
+            DAN.device_registration_with_retry(ServerURL, Reg_addr)
+        else:
+            print('Connection failed due to unknow reasons.')
+            time.sleep(1)    
+    time.sleep(0.2)
